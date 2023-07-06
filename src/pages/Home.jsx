2077be28e7b3bc014a4646c168433e75abe4145f
Nicolae <andronicnicolae15@gmail.com>
2023-07-06 20:31:20 +0300

    added qr code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import Form from '../components/Form'
+import QRCode from 'react-qr-code'
 
 function Home() {
   return (
     <>
       <h1>HOME</h1>
       <Form />
+      <QRCode
+        value={"http://localhost:5173/"}
+      />
     </>
   )
 }
