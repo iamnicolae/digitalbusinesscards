261ef3e2ee72799cd49739a8b7a10146b84c59b8
Nicolae <andronicnicolae15@gmail.com>
2023-08-28 15:04:41 +0300

    style 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,30 @@
+import { styled } from 'styled-components'
+import { Background, Container, Section } from '../styles/template'
+
+const Title = styled.h1`
+  color: white;
+  font-size: 3rem;
+`
+
+const Link = styled.a`
+  color: rgba(255,255,255,0.7);
+  font-weight: 700;
+  display: inline-block;
+  margin: 10px 0;
+`
+
 function NotFound() {
   return (
-    <>
-      <h1>Not found</h1>
-      <p>no profile found please create one at getqr.cc</p>
-    </>
+    <Background>
+      <Container>
+        <Title>Error 404! 😩<br />Page not found.</Title>
+        <div>
+          <Link href={import.meta.env.VITE_APP_URL}>Go to homepage →</Link>
+          <br />
+          <Link href="mailto:nicolae@nicolae.xyz">Get in touch →</Link>
+        </div>
+      </Container>
+    </Background>
   )
 }
 
